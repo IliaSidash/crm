020f70dc61d402b32f13fde411a785a550e17816
Ilia Sidash <iliasidash@gmail.com>
2019-06-03 13:11:09 +0300

    Add firebase

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import App from './App.svelte';
-import '../public/styles/main.css'
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
-const app = new App({
-	target: document.body,
+import '../public/styles/main.css';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDVEGEisV5qHUD51KsWbkU0HSTFOk1Um0E',
+  authDomain: 'todo-svelte.firebaseapp.com',
+  databaseURL: 'https://todo-svelte.firebaseio.com',
+  projectId: 'todo-svelte',
+  storageBucket: 'todo-svelte.appspot.com',
+  messagingSenderId: '630531941164',
+  appId: '1:630531941164:web:2f65739fba18e976'
+};
+let app;
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new App({
+      target: document.body
+    });
+  }
 });
 
 export default app;
