a489fade37aecaeccd10a651d97892ec340bb9fd
Ilia Sidash <iliasidash@gmail.com>
2019-06-15 16:13:12 +0300

    Add categories

--- src/pages/categories.svelte
+++ src/pages/categories.svelte
@@ -1,5 +1,16 @@
 <script>
+  import { onMount } from "svelte";
+
   import Layout from "../layouts/main";
+  import CreateCategory from "../components/create-category";
+  import EditCategory from "../components/edit-category";
+  import Loader from "../components/loader";
+
+  import { category } from "../store";
+
+  onMount(() => {
+    category.fetch();
+  });
 </script>
 
 <Layout>
@@ -11,66 +22,14 @@
       <section>
         <div class="row">
           <div class="col s12 m6">
-            <div>
-              <div class="page-subtitle">
-                <h4>Создать</h4>
-              </div>
-
-              <form>
-                <div class="input-field">
-                  <input id="name" type="text" />
-                  <label for="name">Название</label>
-                  <span class="helper-text invalid">Введите название</span>
-                </div>
-
-                <div class="input-field">
-                  <input id="limit" type="number" />
-                  <label for="limit">Лимит</label>
-                  <span class="helper-text invalid">Минимальная величина</span>
-                </div>
-
-                <button class="btn waves-effect waves-light" type="submit">
-                  Создать
-                  <i class="material-icons right">send</i>
-                </button>
-              </form>
-            </div>
+            <CreateCategory />
           </div>
           <div class="col s12 m6">
-            <div>
-              <div class="page-subtitle">
-                <h4>Редактировать</h4>
-              </div>
-
-              <form>
-                <div class="input-field">
-                  <select>
-                    <option>Category</option>
-                  </select>
-                  <label>Выберите категорию</label>
-                </div>
-
-                <div class="input-field">
-                  <input type="text" id="name" />
-                  <label for="name">Название</label>
-                  <span class="helper-text invalid">TITLE</span>
-                </div>
-
-                <div class="input-field">
-                  <input id="limit" type="number" />
-                  <label for="limit">Лимит</label>
-                  <span class="helper-text invalid">LIMIT</span>
-                </div>
-
-                <button class="btn waves-effect waves-light" type="submit">
-                  Обновить
-                  <i class="material-icons right">send</i>
-                </button>
-              </form>
-            </div>
+            <EditCategory categories={$category} />
           </div>
         </div>
       </section>
     </div>
   </div>
+
 </Layout>
