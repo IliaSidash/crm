7c759d9efc403dbd9fac9ae4d23a999ffbed2193
Ilia Sidash <iliasidash@gmail.com>
2019-06-15 16:02:53 +0300

    Edit category store

--- src/store/category.js
+++ src/store/category.js
@@ -7,8 +7,25 @@ import { getUid } from '../helpers/currentUid';
 function createCategory() {
   const { subscribe, set, update } = writable([]);
 
+  async function fetch() {
+    try {
+      const uid = await getUid();
+      const snapshot = await firebase
+        .database()
+        .ref(`/users/${uid}/categories`)
+        .once('value');
+      const categories = snapshot.val();
+      const normalizedCategories = Object.keys(categories).map((key) => ({
+        id: key,
+        ...categories[key]
+      }));
+      set(normalizedCategories);
+    } catch (e) {}
+  }
+
   return {
     subscribe,
+    fetch,
     async create({ name, limit }) {
       try {
         const uid = await getUid();
@@ -21,6 +38,21 @@ function createCategory() {
           ...prevState,
           { id: category.key, name, limit }
         ]);
+      } catch (e) {
+        const { code } = e;
+        setError(code);
+        throw e;
+      }
+    },
+    async update({ name, limit, id }) {
+      try {
+        const uid = await getUid();
+        await firebase
+          .database()
+          .ref(`/users/${uid}/categories`)
+          .child(id)
+          .update({ name, limit });
+        fetch();
       } catch (e) {
         const { code } = e;
         setError(code);
