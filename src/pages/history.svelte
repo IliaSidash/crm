0e6e2de7b7b5b50f1c52591f6b30db549e52cf12
Ilia Sidash <iliasidash@gmail.com>
2019-06-17 17:06:27 +0300

    Add history page

--- src/pages/history.svelte
+++ src/pages/history.svelte
@@ -1,49 +1,65 @@
 <script>
+  import { onMount } from "svelte";
+  import { links } from "svelte-routing";
+
   import Layout from "../layouts/main";
+  import Loader from "../components/loader";
+  import HistoryTable from "../components/history-table";
+
+  import { category, record } from "../store";
+
+  let records = [];
+  let loading = true;
+
+  function normalizeData() {
+    records = $record.map(record => {
+      const { name } = $category.find(
+        category => category.id === record.categoryId
+      );
+      const recordClass = record.type === "income" ? "green" : "red";
+      const typeText = record.type === "income" ? "Доход" : "Расход";
+
+      return { ...record, categoryName: name, recordClass, typeText };
+    });
+  }
+
+  async function fetchData() {
+    try {
+      await category.fetch();
+      await record.fetchRecords();
+      normalizeData();
+      loading = false;
+    } catch (e) {}
+  }
+
+  onMount(() => {
+    fetchData();
+  });
 </script>
 
 <Layout>
-  <div class="app-page">
+  <div class="app-page" use:links>
     <div>
       <div class="page-title">
         <h3>История записей</h3>
       </div>
 
-      <div class="history-chart">
-        <canvas />
-      </div>
+      {#if loading}
+        <Loader />
+      {:else if !records.length}
+        <p class="center">
+          Записей пока нет.
+          <a href="/record">Добавьте новую запись</a>
+        </p>
+      {:else}
+        <div class="history-chart">
+          <canvas />
+        </div>
 
-      <section>
-        <table>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Сумма</th>
-              <th>Дата</th>
-              <th>Категория</th>
-              <th>Тип</th>
-              <th>Открыть</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>1212</td>
-              <td>12.12.32</td>
-              <td>name</td>
-              <td>
-                <span class="white-text badge red">Расход</span>
-              </td>
-              <td>
-                <button class="btn-small btn">
-                  <i class="material-icons">open_in_new</i>
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </section>
+        <section>
+          <HistoryTable {records} />
+        </section>
+      {/if}
     </div>
   </div>
 </Layout>
